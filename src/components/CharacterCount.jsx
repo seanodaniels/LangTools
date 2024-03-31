9667ab449f9df9bc4dd69fb99ebc45b6a4522558
Sean O'Daniels <sodaniels@gmail.com>
2024-03-30 20:47:48 -0400

    feat: results display in table

--- src/components/CharacterCount.jsx
+++ src/components/CharacterCount.jsx
@@ -11,15 +11,18 @@ const CharacterCount = () => {
   }
 
   const displayCounts = (newObject) => {
-    return JSON.stringify(newObject, null, 2)
+    // const stringified = JSON.stringify(newObject, null, 2)
+    if (newObject) {
+      const newArr = Object.entries(newObject)
+      console.log('newArr', newArr)
+      return newArr
+    }
+
   }
 
   const alphabetize = (newObject) => {
-
-    if (newObject) {
-      
+    if (newObject) {      
       let sortedObject = {}
-
       Object.keys(newObject).sort((a, b) => {
         return a.toLowerCase().localeCompare(b.toLowerCase());
       }).forEach(function(key) {
@@ -44,6 +47,24 @@ const CharacterCount = () => {
     return result
   }
 
+  const DisplayCountTable = ({countArr}) => {
+    if (countArr) {
+    return (
+      <table className="count-result-table">
+        <tr>
+          <th>Character</th>
+          <th>Count</th>
+        </tr>
+      {
+        countArr.map(pair => 
+          <tr key={pair}>
+            {pair.map(item => <td key={item}>{item}</td>)}
+          </tr>)
+      }</table>
+      )
+    }
+  }
+
   useEffect(() => {
     const str = userInput
     const strCaseInsensitive = userInput.toLowerCase()
@@ -54,20 +75,23 @@ const CharacterCount = () => {
   return (
     <div id="page-character-count">
       <h2>Character Count</h2>
-      <p>Enter your text.</p>
-      
+      <p>Enter your text.</p>      
 
       <form>
         <label>
           <textarea value={userInput} onChange={handleTextChange} />
         </label>
       </form>
+
+      <h2>Results</h2>
       
       <div id="count-output">
-        <h2>Case Sensitive</h2>
-        <pre className="results">{displayCounts(alphabetize(countBreakdown))}</pre>
-        <h2>Case Insensitive</h2>
-        <pre className="results">{displayCounts(alphabetize(countBreakdownCI))}</pre>
+          
+        <p><strong>Case Sensitive</strong></p>
+        <DisplayCountTable countArr={displayCounts(alphabetize(countBreakdown))} /> 
+
+        <p><strong>Case Insensitive</strong></p>
+        <DisplayCountTable countArr={displayCounts(alphabetize(countBreakdownCI))} /> 
       </div>
     </div>
   )
